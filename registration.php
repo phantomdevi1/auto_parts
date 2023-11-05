c8e7acd1e6a663ee4c8916103c89fbe73ab4a7d1
phantomdevi1 <gruzdev_ilya16@mail.ru>
2023-11-05 21:58:14 +0300

    working registration and autorization

---
+++ registration.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Регистрация</title>
+  </head>
+  <body>
+  <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usermail = $_POST["user_email"];
+    $userphone = $_POST["user_phone"];
+    $username = $_POST["user_name"];
+    $userpass = $_POST["user_password"];
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "auto_parts";
+
+    $dsn = "mysql:host=$host;dbname=$database";
+    $conn = mysqli_connect($host, $user, $pass, $database);
+
+    // Проверка соединения
+    if (!$conn) {
+        die("Ошибка подключения к базе данных: " . mysqli_connect_error());
+    }
+
+    $query = "SELECT * FROM users WHERE user_name = '$username'"; // Заменил 'username' на 'user_name'
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo '<script>alert("Пользователь с таким именем уже зарегистрирован.");</script>';
+    } else {
+        $accessstatus = 0;
+        $query = "INSERT INTO users (user_email, user_phone, user_name, user_password, access_status) VALUES ('$usermail', '$userphone', '$username', '$userpass', '$accessstatus')";
+        if (mysqli_query($conn, $query)) {
+            echo '<script>alert("Вы зарегистрированы");</script>';
+        } else {
+            echo '<script>alert("Ошибка регистрации");</script>';
+        }
+    }
+    mysqli_close($conn);
+}
+?>
+
+
+    <header class="header_index">
+      <h1 class="heading_text">РЕГИСТРАЦИЯ</h1>
+      <div class="toolbar">
+        <a href="catalog.html">КАТАЛОГ</a>
+        <a href="">КОРЗИНА</a>
+        <a href="">ОТЗЫВЫ</a>
+        <a href="">КОНТАКТЫ</a>
+        <a href="auth.php#section_auth" class="last">ПРОФИЛЬ</a>
+      </div>
+    </header>
+    <form action="" method="POST">
+    <div id="section_registration" class="content">
+      <div class="registration_block">
+        <img src="img/logo_auth.svg" alt="" />
+        <p>Регистрация</p>
+        
+          <input class="input_text" type="text" name="user_email" placeholder="Введите вашу почту"/>
+          <input class="input_text" type="text" name="user_phone" placeholder="Введите ваш номер телефона"/>
+          <input class="input_text" type="text" name="user_name" placeholder="Введите ваше имя"/>
+          <input class="input_text" type="password" name="user_password" placeholder="Введите ваш пароль" id="passwordInputReg"/>
+          <span class="span_check_password">
+          <input class="check_password" type="checkbox" id="showPasswordReg" />
+          <label for="showPasswordReg">Показать пароль</label>
+        </span>
+          <!-- Кнопка для отправки формы -->
+          <input class="registration_btn" type="submit" name="submit" value="Зарегистрироваться">
+        </form>
+
+        <span class="input_account"
+          >Уже есть аккаунт?<a href="auth.php#section_auth">Войти</a></span
+        >
+      </div>
+    </div>
+
+    <script>
+      const passwordInput = document.getElementById("passwordInputReg");
+      const showPasswordCheckbox = document.getElementById("showPasswordReg");
+
+      showPasswordCheckbox.addEventListener("change", function () {
+        passwordInput.type = this.checked ? "text" : "password";
+      });
+    </script>
+  </body>
+</html>
